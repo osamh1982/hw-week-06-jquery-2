1372e6691a2473e6e580ee4a5c08c178c36d8130
osamh1982 <osamh_aljunaid@hotmail.com>
2019-11-30 21:04:29 +0300

    HW-Week07

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('#clicker').click(function() {
+        $('#secret').fadeIn(2500)
+    })});
+
+    $('#clicker').click(function() {
+       $('#secret').slideDown()
+    });
+
+
+    $('#clicker').click(function() {
+    $('#clicker').text("Click me to toggle a hidden secret!") 
+    {
+        $('#secret').fadeToggle()
+    }});
+
+
+    $('#clicker2').click(function() {
+        $('p').slideUp()
+     });
+
+
+
+     $("#clicker3").click(function(){
+        $("#image").attr("src", "https://camo.githubusercontent.com/cd497a9f2fde9bbaabd6af9c35135c55db70f7c5/687474703a2f2f72733238342e70627372632e636f6d2f616c62756d732f6c6c33362f426967737465766538372f476966732f4c697a5f4c656d6f6e5f486967685f466976652e6769667e63323030");
+     });
+
+
+
+    
